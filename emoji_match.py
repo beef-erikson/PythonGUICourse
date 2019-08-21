6acf0928378b83b2fbd0dd44ad6473308b28cbbc
beef-erikson <beef.erikson.studios@gmail.com>
2019-08-21 16:20:25 -0400

    Finished emoji match game

--- emoji_match.py
+++ emoji_match.py
@@ -4,24 +4,38 @@ from guizero import App, Box, Picture, PushButton, Text, warn
 from random import randint
 
 # fields
-gameTimer = 20
-rounds = 0
+game_timer = 20
+rounds = 1
+correct_guesses = 0
+bonus_time = 10
+
 
 # timer countdown - ends game when time runs out
 def counter():
     timer.value = int(timer.value) - 1
     if int(timer.value) == 0:
+        global game_timer
+        global correct_guesses
+        global rounds
         # stops timer and shows game over
         timer.cancel(counter)
         result.text_color = "black"
         result.value = "Game Over"
-        warn("Time Over", "You have ran out of time")
-        # resets timer/text, restarts game, and starts new timer
-        timer.value = gameTimer
+        warn("Time Over", "You have ran out of time!\nFinal Score: " + score.value)
+        # resets variable values
+        timer.value = game_timer
         result.value = ""
+        score.value = "0"      
+        correct_guesses = 0
+        game_timer = 20
+        score_bonus.value = ""
+        # adds to round count
+        rounds += 1
+        # restarts game
         setup_round()
+        # starts new timer
         timer.repeat(1000, counter)
-
+        
 
 # populates list of emojis and shuffles them
 def emoji_list():
@@ -29,15 +43,30 @@ def emoji_list():
     emojis = [os.path.join(emojis_dir, f) for f in os.listdir(emojis_dir) if os.path.isfile(os.path.join(emojis_dir, f))]
     shuffle(emojis)
 
-# sets result value if correct answer or not and creates a new round
+
+# sets result/score values if correct answer or not and creates a new round
 def match_emoji(matched):
+    global correct_guesses
+    # resets score bonus text
+    score_bonus.value = ""
     if matched:
         result.text_color = "green"
         result.value = "correct"
+        score.value = int(score.value) + 1
+        # bonus point for 3 correct guesses in a row
+        correct_guesses += 1
+        if correct_guesses == 3:
+            global game_timer
+            timer.value = int(timer.value) + bonus_time
+            score_bonus.value = "BONUS - 3 correct in a row! " + str(bonus_time) + " added to timer!"
+            correct_guesses = 0
     else:
         result.text_color = "red"
         result.value = "incorrect"
+        score.value = int(score.value) - 1
+        correct_guesses = 0
     setup_round()
+
 
 # sets a round up
 def setup_round():
@@ -64,14 +93,12 @@ def setup_round():
     # sets the correct button to report true (the matched cell)
     buttons[random_button].update_command(match_emoji, [True])
 
-    # adds to round played
-    global rounds
-    rounds += 1
+    # Displays rounds played
     rounds_played.value = "Rounds played: " + str(rounds)
 
 
 # sets up widgets
-app = App("emoji match", width=320, height=530)
+app = App("emoji match", width=400, height=570)
 rounds_played = Text(app, text="Rounds played: " + str(rounds))
 result = Text(app)
 game_box = Box(app)
@@ -96,13 +123,21 @@ for x in range(0,3):
 
 # sets timer text
 extra_features = Box(app)
-timer = Text(extra_features, text="Get Ready")
+timerlabel = Text(extra_features, text="Time Left: ", align="left")
+timer = Text(extra_features, text="Get Ready", align="left")
+
+# sets score and bonus texts
+scoreboard = Box(app)
+label = Text(scoreboard, text="Score: ", align="left")
+score = Text(scoreboard, text="0", align="left")
+bonus = Box(app)
+score_bonus = Text(bonus, text="", color="green")
 
 # sets up a round
 setup_round()
 
 # starts timer
-timer.value = gameTimer
+timer.value = game_timer
 timer.repeat(1000, counter)
 
 # starts app
